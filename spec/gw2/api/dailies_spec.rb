1a8a491e849db1459b1aab93db8ec32435a1228c
Aly Fulton <aly@obeythelime.com>
2017-01-24 14:21:57 -0500

    Change dailies specs This makes them less dependendent on the contents of the fixture, should the fixture ever change. Since the dailies change every day, if the fixture would be updated for a different day and response, the test would fail.

--- spec/gw2/api/dailies_spec.rb
+++ spec/gw2/api/dailies_spec.rb
@@ -2,13 +2,13 @@ describe GW2::API::Dailies do
   subject{GW2.client}
   describe ".dailies", :vcr do
     it "gets the dailies for today" do
-      expect(subject.dailies[:pve][0][:id]).to be 1972
+      expect(subject.dailies[:pve][0][:id]).to be_an Integer
     end
   end
 
   describe ".dailies_tomorrow", :vcr do
     it "gets the dailies for tomorrow" do
-      expect(subject.dailies_tomorrow[:pve][0][:id]).to be 1969
+      expect(subject.dailies_tomorrow[:pve][0][:id]).to be_an Integer
     end
   end
 end
